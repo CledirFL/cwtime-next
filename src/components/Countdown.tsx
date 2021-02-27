55ea4d06a3aa1eea3d608b272b7d3db910736845
Kelton <keltonfernandes@live.com>
2021-02-27 11:46:58 -0800

    create component count down

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,22 @@
+import styles from "../styles/components/Countdown.module.css";
+
+export default function Countdown() {
+    return (
+        <div>
+            <div className={styles.container}>
+                <div>
+                    <span>2</span>
+                    <span>5</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>0</span>
+                    <span>0</span>
+                </div>
+            </div>
+            <button type="button" className={styles.button}>
+                Iniciar um ciclo
+            </button>
+        </div>
+    )
+}
