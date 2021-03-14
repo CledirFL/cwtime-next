ef9677a532120856e289ac17ec5d2b0c8d4bd85a
Kelton <keltonfernandes@live.com>
2021-03-14 15:40:12 -0700

    add component challengebox

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,49 @@
+import styles from "../styles/components/ChallengeBox.module.css"
+
+function ChallengeBox() {
+
+    const hasActiveChallenge = true;
+
+    return (
+        <div className={styles.container}>
+            { hasActiveChallenge ?
+                (<div className={styles.active} >
+                    <header>Ganhe 400 xp</header>
+
+                    <main>
+                        <img src="icons/body.svg" />
+                        <strong>Novo desafio</strong>
+                        <p>Levante e faça uma caminhada de 3 minutos.</p>
+                    </main>
+
+                    <footer>
+                        <button
+                            type="button"
+                            className={styles.failedButton}
+                        >
+                            Falhei
+                        </button>
+                        <button
+                            type="button"
+                            className={styles.succeededButton}
+                        >
+                            Completei
+                        </button>
+                    </footer>
+                </div>)
+                :
+                (
+                    <div className={styles.notActive}>
+                        <strong>Finalize um ciclo para receber um desafio</strong>
+                        <p>
+                            <img src="icons/level-up.svg" alt="Level up" />
+                            Avance de level completando desafios.
+                        </p>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
+
+export default ChallengeBox;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import Head from "next/head";
+
 import CompletedChallenges from "../components/CompletedChallenges";
 import Countdown from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from "../components/Profile";
+import ChallengeBox from "../components/ChallengeBox";
 
-import Head from "next/head";
+
 
 import styles from "../styles/pages/Home.module.css";
 
@@ -21,7 +24,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown/>
         </div>
-        <div>d</div>
+        <div>
+          <ChallengeBox/>
+        </div>
       </section>
     </div>
   )
