00c52e8738fff5afeba4a43ea54d9a3f9535aa6c
Kelton <keltonfernandes@live.com>
2021-03-14 17:34:06 -0700

    add level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,11 @@
 
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export default function Profile() {
+    const {level} = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/CledirFL.png" alt="Kelton Cabral"/>
@@ -9,7 +13,7 @@ export default function Profile() {
                 <strong>Kelton Cabral</strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
